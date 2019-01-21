765346dd6ac701e768e70eabe63b981ba0f7d23b
Aaron Berns <anberns@gmail.com>
2019-01-21 15:28:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,6 +66,12 @@ class Dog
 		dog
 	end
 	
-	def self.new_from_db
+	def self.new_from_db(row)
+	  new_dog = self.new
+		new_dog.id = row[0]
+		new_dog.name =  row[1]
+		new_dog.grade = row[2]
+		new_dog
+	end
   
 end
